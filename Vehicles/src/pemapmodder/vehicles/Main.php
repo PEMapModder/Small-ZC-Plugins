4193c3fba65c3d5a267e483407803bbf3230cd67
PEMapModder <pemapmodder1970@gmail.com>
2014-06-01 17:47:17 +0800

    Base things.
    The plugin is now working!

---
+++ Vehicles/src/pemapmodder/vehicles/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace pemapmodder\vehicles;
+
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender as Issuer;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase as Base;
+
+class Main extends Base implements Listener{
+}
