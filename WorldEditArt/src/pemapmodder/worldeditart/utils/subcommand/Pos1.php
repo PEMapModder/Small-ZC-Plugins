f0bd48bd5fa4077d314a77468b3bdd68cb9fecef
PEMapModder <pemapmodder1970@gmail.com>
2014-07-27 17:49:23 +0800

    Added /w pos1

---
+++ WorldEditArt/src/pemapmodder/worldeditart/utils/subcommand/Pos1.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pemapmodder\worldeditart\utils\subcommand;
+
+use pocketmine\Player;
+
+class Pos1 extends Subcommand{
+	public function getName(){
+		return "pos1";
+	}
+	public function getDescription(){
+
+	}
+	public function getUsage(){
+		return "/w pos1";
+	}
+	public function checkPermission(Player $player){
+
+	}
+}
