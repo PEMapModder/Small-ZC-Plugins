b239a0da31a7a242adb1527dd1934a6b0fa799fb
PEMapModder <pemapmodder1970@gmail.com>
2014-10-05 08:28:50 +0800

    NailedKeyboard: Line.php new commits, allows selections

--- NailedKeyboard/src/pemapmodder/nailedkeyboard/Line.php
+++ NailedKeyboard/src/pemapmodder/nailedkeyboard/Line.php
@@ -7,8 +7,46 @@ class Line{
 	private $text;
 	/** @var int */
 	private $pointer, /** @noinspection PhpUnusedPrivateFieldInspection */ $selectFrom = null;
+	/** @var string */
+	private $clipboard = null;
 	public function __construct($text = "", $pointer = 0){
 		$this->text = $text;
+		$this->pointer = $pointer;
+		$this->selectFrom = null;
+	}
+	public function startSelection(){
+		$this->selectFrom = $this->pointer;
+	}
+	public function deselect(){
+		$this->selectFrom = $this->pointer;
+	}
+	public function copy(){
+		if($this->selectFrom = null){
+			throw new \BadMethodCallException;
+		}
+		$this->clipboard = self::substr($this->text, min($this->pointer, $this->selectFrom), max($this->pointer, $this->pointer));
+	}
+	public function cut(){
+		if($this->selectFrom === null){
+			throw new \UnexpectedValueException;
+		}
+		$from = min($this->pointer, $this->selectFrom);
+		$to = max($this->pointer, $this->selectFrom);
+		$this->clipboard = self::substr($this->text, $from, $to);
+		$this->text = self::substr($this->text, 0, $from) . self::substr($this->text, $to);
+		$this->selectFrom = null;
+		$this->pointer = $from;
+	}
+	public function paste(){
+		if($this->clipboard === null){
+			throw new \UnexpectedValueException;
+		}
+		$this->input($this->clipboard);
+	}
+	public function clearClipboard(){
+		$copy = $this->clipboard;
+		$this->clipboard = null;
+		return $copy;
 	}
 	public function left(){
 		if($this->pointer === 0){
@@ -23,6 +61,12 @@ class Line{
 		$this->pointer++;
 	}
 	public function backspace(){
+		if($this->selectFrom !== null){
+			$this->text = self::substr($this->text, 0, min($this->pointer, $this->selectFrom)) . self::substr($this->text, max($this->pointer, $this->selectFrom));
+			$this->pointer = min($this->pointer, $this->selectFrom);
+			$this->selectFrom = null;
+			return;
+		}
 		if($this->pointer === 0){
 			throw new \OutOfBoundsException;
 		}
@@ -33,6 +77,12 @@ class Line{
 		$this->pointer--;
 	}
 	public function delete(){
+		if($this->selectFrom !== null){
+			$this->text = self::substr($this->text, 0, min($this->pointer, $this->selectFrom)) . self::substr($this->text, max($this->pointer, $this->selectFrom));
+			$this->pointer = min($this->pointer, $this->selectFrom);
+			$this->selectFrom = null;
+			return;
+		}
 		if($this->pointer === self::strlen($this->text)){
 			throw new \OutOfBoundsException;
 		}
@@ -41,6 +91,11 @@ class Line{
 		$this->text = $left . $right;
 	}
 	public function input($input){
+		if($this->selectFrom !== null){
+			$this->text = self::substr($this->text, 0, min($this->pointer, $this->selectFrom)) . self::substr($this->text, max($this->pointer, $this->selectFrom));
+			$this->pointer = min($this->pointer, $this->selectFrom);
+			$this->selectFrom = null;
+		}
 		$left = self::substr($this->text, 0, $this->pointer);
 		$right = self::substr($this->text, $this->pointer);
 		$this->text = $left . $input . $right;
