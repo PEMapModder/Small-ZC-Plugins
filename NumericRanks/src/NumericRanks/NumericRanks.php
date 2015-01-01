fe14d8b1f81736e7a8bdc613605aa680a50ef1d1
64FF00 <64ff00@gmail.com>
2015-01-01 15:51:42 +0900

    NumericRanks: My first commit

--- NumericRanks/src/NumericRanks/NumericRanks.php
+++ NumericRanks/src/NumericRanks/NumericRanks.php
@@ -4,8 +4,19 @@ namespace NumericRanks;
 
 use pocketmine\plugin\PluginBase;
 
-class NumericRanks extends PluginBase{
-	public function onEnable(){
+class NumericRanks extends PluginBase
+{
+	private $cfg;
+	
+	public function onEnable()
+	{
+		$this->loadConfig();
+	}
+	
+	public function loadConfig()
+	{
 		$this->saveDefaultConfig();
+		
+		$this->cfg = $this->getConfig();
 	}
 }
