762c8f7c832a07a449c2d1645f90ba8e699270f8
SOFe <sofe2038@gmail.com>
2017-03-10 16:22:12 +0800

    SimpleMacros: Version 1.0 updates

--- SimpleMacros/src/pemapmodder/simplemacros/Main.php
+++ SimpleMacros/src/pemapmodder/simplemacros/Main.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ * SimpleMacros
+ * Copyright (C) 2015-2017 PEMapModder and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 namespace pemapmodder\simplemacros;
 
 use pocketmine\command\Command;
@@ -13,21 +32,28 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
-	private $stack = [];
-	private $paused = [];
+	/** @var RecordSession[] */
+	private $recordSessions = [];
 	/** @var \pocketmine\permission\PermissionAttachment[] */
 	private $atts = [];
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		@mkdir($this->getDataFolder());
-		@mkdir($this->getDataFolder() . "macros/");
+		if(!is_dir($d = $this->getDataFolder())){
+			mkdir($d);
+		}
+		if(!is_dir($d .= "macros/")){
+			mkdir($d);
+		}
+		foreach($this->getServer()->getOnlinePlayers() as $player){
+			$this->initPlayer($player);
+		}
 	}
 
 	/**
 	 * @param PlayerCommandPreprocessEvent $event
 	 *
-	 * @priority        MONITOR
+	 * @priority        HIGH
 	 * @ignoreCancelled true
 	 */
 	public function onPreCmd(PlayerCommandPreprocessEvent $event){
@@ -35,7 +61,9 @@ class Main extends PluginBase implements Listener{
 		if(substr($line, 0, 1) !== "/"){
 			return;
 		}
-		$this->onCmd($event->getPlayer()->getID(), substr($line, 1));
+		if($this->commandPreprocess($event->getPlayer()->getID(), substr($line, 1))){
+			$event->setCancelled();
+		}
 	}
 
 	/**
@@ -45,194 +73,285 @@ class Main extends PluginBase implements Listener{
 	 * @ignoreCancelled true
 	 */
 	public function onConsoleCmd(ServerCommandEvent $event){
-		$this->onCmd("pocketmine\\command\\ConsoleCommandSender", $event->getCommand());
+		if($this->commandPreprocess("pocketmine\\command\\ConsoleCommandSender", $event->getCommand())){
+			$event->setCancelled();
+		}
 	}
 
-	private function onCmd($id, $line){
+	/**
+	 * Handles a command execution event by any command senders
+	 *
+	 * @param string $id   identifier of command sender
+	 * @param string $line command executed, without the slash
+	 * @return bool whether the event should be cancelled
+	 */
+	public function commandPreprocess(string $id, string $line) : bool{
 		if(strtolower(substr($line, 0, strlen("macro"))) === "macro" and strtolower(substr($line, 0, strlen("macro run"))) !== "macro run"){
-			return;
+			return false;
 		}
-		if(isset($this->stack[$id]) and @$this->paused[$id] !== true){
-			$this->stack[$id] .= "$line\n";
+
+		if(isset($this->recordSessions[$id])){
+			return $this->recordSessions[$id]->handle($line);
 		}
+		return false;
 	}
 
+	/**
+	 * @param PlayerJoinEvent $event
+	 * @priority MONITOR
+	 */
 	public function onJoin(PlayerJoinEvent $event){
-		$this->atts[$event->getPlayer()->getID()] = $event->getPlayer()->addAttachment($this);
+		$this->initPlayer($event->getPlayer());
 	}
 
+	/**
+	 * @param PlayerQuitEvent $event
+	 * @priority MONITOR
+	 */
 	public function onQuit(PlayerQuitEvent $event){
 		$this->finalizePlayer($event->getPlayer(), true);
+	}
+
+	public function initPlayer(Player $player){
+		$this->atts[$player->getId()] = $player->addAttachment($this);
+	}
+
+	public function finalizePlayer(Player $player, $isQuit){
+		if(isset($this->recordSessions[$player->getId()])){
+			if(!$isQuit){
+				$player->sendMessage("Your recording macro has been discarded due to SimpleMacros being disabled.");
+			}
+			unset($this->recordSessions[$player->getId()]);
+		}
+		if(isset($this->atts[$player->getId()])){
+			unset($this->atts[$player->getId()]);
+		}
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, $alias, array $args){
 		if(!isset($args[0])){
 			return false;
 		}
-		$k = ($sender instanceof Player) ? $sender->getID() : get_class($sender);
-		switch($subcmd = array_shift($args)){
-			case "stop":
-				if(!isset($this->stack[$k])){
-					$sender->sendMessage("You don't have a recording macro to stop!");
-					break;
-				}
-				if(!isset($args[0])){
-					$sender->sendMessage("Usage: /macro stop <name>");
-					$sender->sendMessage("Don't worry, this command will not be recorded.");
-					break;
-				}
-				if(strtolower($name = array_shift($args)) === "ng"){
-					$sender->sendMessage("This NG macro has been discarded.");
-					// continue to run: unset stack
-				}else{
-					if($this->getMacro($name) !== false){
-						$sender->sendMessage("There is already a macro called \"$name\"!");
-						$sender->sendMessage("Don't worry, this command will not be recorded.");
-						break;
-					}
-					$success = $this->saveMacro($name, $this->stack[$k]);
-					if(!$success){
-						$sender->sendMessage("Unable to create file. Perhaps \"$name\" is not a valid filename?");
-						$sender->sendMessage("Don't worry, this command will not be recorded.");
-						break;
-					}
-					// continue to run: unset stack
-				}
-				if(isset($this->stack[$id = $k])){ // copied xD
-					unset($this->stack[$id]); // avoid bugs if the entity ID gets reused
-				}
-				if(isset($this->paused[$id])){
-					unset($this->stack[$id]);
-				}
-				$sender->sendMessage("You have successfully saved macro $name.");
-				break;
+		$id = ($sender instanceof Player) ? $sender->getId() : get_class($sender);
+		switch($subcmd = strtolower(array_shift($args))){
+			case "s":
 			case "start":
-				if(!$sender->hasPermission("simplemacros.record")){
-					$sender->sendMessage("You don't have permission to record a macro.");
-					return true;
-				}
-				if(isset($this->stack[$k])){
-					$sender->sendMessage("You have already started recording a macro!");
-					return true;
-				}
-				$this->stack[$k] = "";
-				$sender->sendMessage("You are now recording a macro.");
-				break;
+				$this->startRecord($sender, $id, $args);
+				return true;
+			case "p":
 			case "pause":
-				if(!isset($this->stack[$k])){
-					$sender->sendMessage("You are not recording a macro!");
-					return true;
-				}
-				$this->paused[$k] = true;
-				$sender->sendMessage("Your macro is now paused.");
-				$sender->sendMessage("Don't worry, this command will not be recorded.");
-				break;
+				$this->pauseRecord($sender, $id);
+				return true;
+			case "c":
+			case "cont":
+			case "continue":
 			case "resume":
-				if(!isset($this->paused[$k]) or $this->paused[$k] === false){
-					$sender->sendMessage("You do not have a paused recording macro!");
-					$sender->sendMessage("Don't worry, this command will not be recorded.");
-					return true;
-				}
-				$this->paused[$k] = false;
-				$sender->sendMessage("You have now resumed your macro.");
-				break;
-			case "sudo":
-				if(!$sender->hasPermission("simplemacros.sudo")){
-					$sender->sendMessage("You don't have permision to sudo others with a macro.");
-					return true;
-				}
-				if(!isset($args[1])){
-					$sender->sendMessage("Usage: /macro sudo <macro name> <player name> [-op]");
-					return true;
-				}
-				$macro = $this->getMacro($name = array_shift($args));
-				if($macro === false){
-					$sender->sendMessage("$macro doesn't exist!");
-					return true;
-				}
-				if(!(($p = $this->getServer()->getPlayer($args[1])) instanceof Player)){
-					$sender->sendMessage("Player $args[1] not found!");
-					return true;
-				}
-				if(isset($args[2]) and $args[2] === "-op" and !$sender->hasPermission("simplemacros.opsudo")){
-					$sender->sendMessage("You do not have permission to op-sudo a player with a macro!");
-				}
-				foreach($macro as $rline){
-					$line = trim($rline);
-					if(isset($args[2]) and $args[2] === "-op"){
-						if(($rcmd = $this->getServer()->getCommandMap()->getCommand(strstr($line, " ", true))) instanceof Command){
-							if(!$cmd->testPermissionSilent($p) and $cmd->testPermissionSilent($sender)){
-								$perms = [$rcmd->getPermission()];
-								if(strpos($perms[0], ";") !== false){
-									$perms = explode(";", $perms[0]);
-								}
-								foreach($perms as $perm){
-									$this->atts[$k]->setPermission($perm, true);
-								}
-							}
-						}
-					}
-					$this->getServer()->dispatchCommand($p, $line);
-					if(isset($perms)){
-						foreach($perms as $perm){
-							$this->atts[$k]->unsetPermission($perm);
-						}
-					}
-				}
-				$sender->sendMessage("Command run as " . $p->getName() . ".");
-				break;
+				$this->resumeRecord($sender, $id);
+				return true;
+			case "q":
+			case "stop":
+				$this->stopRecord($sender, $id, $args);
+				return true;
+			case "x":
+			case "exe":
+			case "exec":
+			case "execute":
+			case "r":
 			case "run":
-				if(!$sender->hasPermission("simplemacros.run")){
-					$sender->sendMessage("You don't have permission to run a macro.");
-					return true;
-				}
-				if(!isset($args[0])){
-					$sender->sendMessage("Usage: /macro run <name>");
-					return true;
-				}
-				foreach($this->getMacro($args[0]) as $line){
-					$this->getServer()->dispatchCommand($sender, $line);
-				}
-				break;
+				$this->runMacro($sender, $args);
+				return true;
+			case "u":
+			case "su":
+			case "sudo":
+				$this->sudoMacro($sender, $args);
+				return true;
 			default:
-				return false;
+				$sender->sendMessage(/** @lang text */
+					<<<EOM
+Usage: /macro start|pause|resume|stop|run|sudo
+/macro start [t]: Start recording a macro.
+    Add a "t" after the command to execute commands while recording the macro.
+    Otherwise, unless you stopped or paused recording macros, you can't run any commands.
+    /macro commands are the only exception -- they won't be recorded nor blocked.
+    Aliases: /macro s
+/macro pause: Pause recording a macro.
+    Aliases: /macro p
+/macro resume: Resume recording a macro.
+    Aliases: /macro c, /macro cont, /macro continue
+/macro stop [.f] <name>: Stop recording a macro and save it.
+    Using "ng" as the name will prevent saving.
+    Adding .f before the name will overwrite the original macro with the same name, if any.
+    Aliases: /macro q
+/macro run <macro>: Run a macro saved by yourself or others.
+    Aliases: /macro r, /macro x, /macro exe, /macro exec, /macro execute
+/macro su <macro> <target> [.op]: Run a macro as another player
+    Add .op after the command to bypass all permission barriers (the player will be temporarily granted all permissions). 
+    Otherwise, some commands may not be executed, but instead show permission-denied messages.
+    Aliases: /macro sudo
+EOM
+				);
+				return true;
 		}
-		return true;
 	}
 
-	public function saveMacro($name, $lines){
-		$success = file_put_contents($this->getDataFolder() . "macros/$name.txt", $lines);
-		return ($success !== false);
+	public function startRecord(CommandSender $sender, string $id, array $args){
+		if(!$sender->hasPermission("simplemacros.record")){
+			$sender->sendMessage("You don't have permission to record a macro.");
+			return;
+		}
+		if(isset($this->recordSessions[$id])){
+			$sender->sendMessage("You have already started recording a macro!");
+			return;
+		}
+		$this->recordSessions[$id] = $session = new RecordSession(in_array("t", $args));
+		$sender->sendMessage("Now recording a macro. /macro commands will not be recorded.");
+		$sender->sendMessage($session->tee ? "Commands you type will be executed in addition to being recorded into the macro."
+			: "Commands you type will be recorded into the macro but will not be executed.");
 	}
 
-	public function getMacro($name){
+	public function pauseRecord(CommandSender $sender, string $id){
+		if(!isset($this->recordSessions[$id])){
+			$sender->sendMessage("You are not recording a macro!");
+			return;
+		}
+		if($this->recordSessions[$id]->paused){
+			$sender->sendMessage("Your macro has already been stopped!");
+			return;
+		}
+		$this->recordSessions[$id]->paused = true;
+		$sender->sendMessage("You have paused recording a macro.");
+	}
+
+	public function resumeRecord(CommandSender $sender, string $id){
+		if(!isset($this->recordSessions[$id])){
+			$sender->sendMessage("You are not recording a macro!");
+			return;
+		}
+		if(!$this->recordSessions[$id]->paused){
+			$sender->sendMessage("Your macro was not paused!");
+			return;
+		}
+		$this->recordSessions[$id]->paused = false;
+		$sender->sendMessage("You have resumed recording a macro.");
+	}
+
+	public function stopRecord(CommandSender $sender, string $id, array $args){
+		if(!isset($this->recordSessions[$id])){
+			$sender->sendMessage("You don't have a recording macro to stop!");
+			return;
+		}
+		if(!isset($args[0])){
+			$sender->sendMessage("Usage: /macro stop [.f] <name>");
+			return;
+		}
+		if(strtolower($name = array_shift($args)) === "ng"){
+			$sender->sendMessage("This macro has been discarded.");
+			// continue to run: unset stack
+		}else{
+			if(strtolower($name) === ".f"){
+				$overwrite = true;
+				$name = array_shift($args);
+			}
+			if(isset($overwrite) or $this->hasMacro($name)){
+				$sender->sendMessage("There is already a macro called \"$name\"!");
+				$sender->sendMessage("Please use \"/macro stop\" again with another name.");
+				$sender->sendMessage("Use \"/macro stop ow $name\" to overwrite the old macro.");
+				return;
+			}
+			$success = $this->recordSessions[$id]->save($this, $name);
+			if(!$success){
+				$sender->sendMessage("Unable to create file. Perhaps \"$name\" is not a valid filename?");
+				$sender->sendMessage("Please use \"/macro stop\" again with another name.");
+				return;
+			}
+			// continue to run: unset stack
+		}
+		if(isset($this->recordSessions[$id])){
+			unset($this->recordSessions[$id]);
+		}
+		$sender->sendMessage("You have successfully saved macro \"$name\".");
+	}
+
+	public function runMacro(CommandSender $sender, array $args){
+		if(!$sender->hasPermission("simplemacros.run")){
+			$sender->sendMessage("You don't have permission to run a macro.");
+			return;
+		}
+		if(!isset($args[0])){
+			$sender->sendMessage("Usage: /macro run <name>");
+			return;
+		}
+		$name = array_shift($args);
+		foreach($this->getMacro($name) as $line){
+			$this->getServer()->dispatchCommand($sender, $line);
+		}
+	}
+
+	public function sudoMacro(CommandSender $sender, array $args){
+		if(!$sender->hasPermission("simplemacros.sudo")){
+			$sender->sendMessage("You don't have permision to sudo others with a macro.");
+			return;
+		}
+		if(!isset($args[1])){
+			$sender->sendMessage("Wrong usage! Run \"macro\" for detailed usage.");
+			return;
+		}
+		try{
+			$macro = $this->getMacro($name = array_shift($args));
+		}catch(\RuntimeException $e){
+			$sender->sendMessage("Macro doesn't exist.");
+			return;
+		}
+		$target = $this->getServer()->getPlayer($targetName = array_shift($args));
+		if(!($target instanceof Player)){
+			$sender->sendMessage("Player \"$targetName\" not found.");
+			return;
+		}
+		if(strtolower($target->getName()) !== strtolower($targetName)){
+			$sender->sendMessage("Interpreted \"$targetName\" as \"{$target->getName()}\".");
+		}
+		$asOp = isset($args[0]) && $args[0] === ".op";
+		if($asOp && !$sender->hasPermission("simplemacros.opsudo")){
+			$sender->sendMessage("You do not have permission to op-sudo a player with a macro!");
+			return;
+		}
+		if($asOp){
+			$perms = [];
+			foreach($this->getServer()->getPluginManager()->getPermissions() as $perm){
+				if(!$target->hasPermission($permName = $perm->getName())){
+					$perms[$permName] = true;
+				}
+			}
+			$this->atts[$target->getId()]->setPermissions($perms);
+		}
+		foreach($macro as $line){
+			$line = trim($line);
+			$this->getServer()->dispatchCommand($target, trim($line));
+		}
+		if($asOp and isset($perms)){
+			$this->atts[$target->getId()]->unsetPermissions(array_keys($perms));
+		}
+		$sender->sendMessage(count($macro) . " commands run as " . $target->getName() . ($asOp ? " bypassing permission limits" : "") . ".");
+	}
+
+	/**
+	 * @param string $name
+	 * @return string[]
+	 */
+	public function getMacro(string $name) : array{
 		if(is_file($path = $this->getDataFolder() . "macros/$name.txt")){
 			return explode("\n", file_get_contents($path));
 		}
-		return false;
+		throw new \RuntimeException("Not found");
+	}
+
+	public function hasMacro(string $name) : bool{
+		return is_file($this->getDataFolder() . "macros/$name.txt");
 	}
 
 	public function onDisable(){
 		foreach($this->getServer()->getOnlinePlayers() as $p){
 			$this->finalizePlayer($p, false);
-		}
-	}
-
-	public function finalizePlayer(Player $player, $isQuit){
-		if(isset($this->stack[$id = $player->getID()])){
-			unset($this->stack[$id]); // avoid bugs if the entity ID gets reused
-			if($isQuit){
-				$this->getLogger()->alert($player->getDisplayName() . " has quit, so his recording macro has been discarded.");
-			}else{
-				$this->getLogger()->alert("Discarding recording macro of " . $player->getDisplayName() . " due to plugin disable.");
-				$player->sendMessage("Your recording macro has been discarded due to SimpleMacros being disabled.");
-			}
-		}
-		if(isset($this->paused[$id])){
-			unset($this->stack[$id]);
-		}
-		if(isset($this->atts[$id])){
-			unset($this->atts[$id]);
 		}
 	}
 }
---
+++ SimpleMacros/src/pemapmodder/simplemacros/RecordSession.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * SimpleMacros
+ * Copyright (C) 2015-2017 PEMapModder and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace pemapmodder\simplemacros;
+
+class RecordSession{
+	public $stack = [];
+	public $paused;
+	public $tee;
+
+	/**
+	 * RecordSession constructor.
+	 *
+	 * @param bool $tee whether the command should be executed (true) or cancelled (false)
+	 */
+	public function __construct(bool $tee){
+		$this->paused = false;
+		$this->tee = $tee;
+	}
+
+	public function handle(string $line) : bool{
+		if($this->paused){
+			return false;
+		}
+		$this->stack[] = $line;
+		return !$this->tee;
+	}
+
+	public function save(Main $main, string $name) : bool{
+		return file_put_contents($main->getDataFolder() . "macros/$name.txt", implode("\n", $this->stack)) != false;
+	}
+}
